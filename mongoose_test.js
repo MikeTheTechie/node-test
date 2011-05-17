b481e58b044cd8df1822a92ef64e575ef9eb313b
Mike Austin <mike2010@austin-uk.co.uk>
2011-05-17 15:09:31 +0100

    Cleaned up models and added a few more comments.

--- mongoose_test.js
+++ mongoose_test.js
@@ -25,28 +25,30 @@ var BlogPost = new Schema({
     }
 });
 
-mongoose.model('Comments', Comments);
+// define the model for the collection 
 mongoose.model('BlogPost', BlogPost);
 
 mongoose.connect('mongodb://localhost/my_blogs');
 // Once we define a model through `mongoose.model('ModelName', mySchema)`, we can access it through the same function
 
-    var myBlogPost = mongoose.model('BlogPost');
+var myBlogPost = mongoose.model('BlogPost');
 
 // We can then instantiate it, and save it:
 
-    var post = new myBlogPost();
-    post.author='mike';
+var post = new myBlogPost();
+post.author='mike';
 
-    // create a comment
-    post.comments.push({ title: 'My comment', body: 'My body text' });
-    
-    post.save(function (err) {
-    	if (err) {
-    		sys.debug("Error returned by save.  " + err );
-    	}
-    	else
-    		{sys.debug("Worked!");};
-    	process.exit(1);
-      //
-    });
+// create a comment
+post.comments.push({ title: 'My comment', body: 'My body text' });
+
+// save the document to the collection
+post.save(function (err) {
+	if (err) {
+		sys.debug("Error returned by save.  " + err );
+	}
+	else
+		{sys.debug("Worked!");};
+	// close the node process. I'm not 100% sure why this is necessary - todo review/investigate.
+	process.exit(1);
+  //
+});
